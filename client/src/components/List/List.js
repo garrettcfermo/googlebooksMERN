70cf1bed09b8f16bac7e2231e5b5e6e782ab581e
Garrett Fermo <garrettcfermo@gmail.com>
2018-12-28 21:36:11 -0800

    Routers and Boiler Components Setup

---
+++ client/src/components/List/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+
+  render() {
+
+    return (
+      <>
+        <h1>This is the List Page!</h1>
+      </>
+    )
+  }
+}
+
+export default List
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+
+  render() {
+
+    return (
+      <>
+        <h1>This is the Navbar Page!</h1>
+      </>
+    )
+  }
+}
+
+export default Navbar
